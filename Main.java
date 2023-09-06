0d0d9ab24a4c5d43f388ab3fa1bb7ab050c94298
NeutralUsername <stuck.fabian@gmail.com>
2023-09-06 13:28:22 +0200

    listener with random names

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main {
        public static void main(String[] args) {
         Server server = new Server(4711);
-        for (int i = 0; i < 10; i++) {
-            String connectionId = "testname"+i;
-            server.acceptConnection(connectionId);
-            server.closeConnection(connectionId);
-        }
+        server.listenForConnections();
+
+        //unreachable currently
         server.closeServer();
     }
 }
--- Server.java
+++ Server.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Server {
     private HashMap<String, Connection> connections;
@@ -16,6 +18,15 @@ public class Server {
         }
     }
 
+    public void listenForConnections() {
+        while (true) {
+            byte[] array = new byte[7]; // length is bounded by 7
+            new Random().nextBytes(array);
+            String rndString = new String(array, Charset.forName("UTF-8"));
+            acceptConnection(rndString);
+        }
+    }
+
     public void acceptConnection(String id) {
         try {
             Socket socket = server.accept();
@@ -24,7 +35,7 @@ public class Server {
                 System.out.println("Connection with id " + id + " already exists!");
                 return;
             }
-            
+
             Connection connection = new Connection(socket, id);
             connections.put(id, connection);
             connection.send("Hello " + id + "!");
