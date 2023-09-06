ae9af17448a2d242a6b867c2f2876ab701a41606
NeutralUsername <stuck.fabian@gmail.com>
2023-09-06 12:06:49 +0200

    basic networking

--- Server.java
+++ Server.java
@@ -1,18 +1,26 @@
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            String name = "playnodge.com";
-            System.out.println("Host: " + name);
-            InetAddress host = InetAddress.getByName(name);
-            String address = host.getHostAddress();
-            System.out.println("IP des Hosts: " + address);
-            byte[] ip = host.getAddress();
-            InetAddress inetaddress = InetAddress.getByAddress(ip);
-            System.out.println("Hostname aus IP: " +
-                    inetaddress.getHostName());
-        } catch (Exception e) {
+            ServerSocket server = new ServerSocket(4711);
+            System.out.println("Server: waiting for connection");
+            Socket client = server.accept();
+            System.out.println("Server: connected to Client " +
+                    client.getInetAddress());
+            OutputStream stream = client.getOutputStream();
+            String message = "Hello Client";
+            byte bmessage[] = message.getBytes();
+            stream.write(bmessage);
+            stream.flush();
+            stream.close();
+            client.close();
+            server.close();
+        } catch (IOException e) {
             // Generelles Exception-Handling zu Demo-Zwecken
             e.printStackTrace();
         }
