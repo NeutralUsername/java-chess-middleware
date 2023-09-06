8b8437577075b9fd2ec73352b053658ea0dcda65
NeutralUsername <stuck.fabian@gmail.com>
2023-09-06 11:54:57 +0200

    test

--- Server.java
+++ Server.java
@@ -1,6 +1,20 @@
- 
-public class Server  {
+import java.net.InetAddress;
+
+public class Server {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        try {
+            String name = "playnodge.com";
+            System.out.println("Host: " + name);
+            InetAddress host = InetAddress.getByName(name);
+            String address = host.getHostAddress();
+            System.out.println("IP des Hosts: " + address);
+            byte[] ip = host.getAddress();
+            InetAddress inetaddress = InetAddress.getByAddress(ip);
+            System.out.println("Hostname aus IP: " +
+                    inetaddress.getHostName());
+        } catch (Exception e) {
+            // Generelles Exception-Handling zu Demo-Zwecken
+            e.printStackTrace();
+        }
     }
 }
