7cc7a4d9d70ca4864dfd06a7c47d5c9a6407fe2f
NeutralUsername <stuck.fabian@gmail.com>
2023-09-06 12:55:40 +0200

    more connections!

--- Server.java
+++ Server.java
@@ -5,9 +5,11 @@ public class Server {
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(4711);
-            Connection connection = new Connection(server.accept(), "testname1");
-            connection.send("Hello Client");    
-            connection.close();
+            for (int i = 0; i < 10; i++) {
+                Connection connection = new Connection(server.accept(), "testname" + i);
+                connection.send("Hello Client " + i + "!");    
+                connection.close();
+            }
             server.close();
         } catch (IOException e) {
             e.printStackTrace();
