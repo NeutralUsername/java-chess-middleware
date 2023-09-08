1dadb9108c7e72a46d62215ea7df6b2ea8f2b4e6
NeutralUsername <stuck.fabian@gmail.com>
2023-09-08 12:13:41 +0200

    castling test cases

--- Main.java
+++ Main.java
@@ -58,43 +58,147 @@ public class Main {
          * chess.printBoard();
          */
 
-        //castling (queen side)
-        chess.move(0, 1, 2, 2);
-        chess.printBoard();
-
-        chess.move(6, 4, 4, 4);
-        chess.printBoard();
-
-        chess.move(1, 3, 3, 3);
-        chess.printBoard();
-
-        chess.move(7, 3, 4, 6);
-        chess.printBoard();
-
-        chess.move(1, 1, 3, 1);
-        chess.printBoard();
-
-        chess.move(6, 0, 5, 0);
-        chess.printBoard();
-
-        chess.move(0, 2, 2, 0);
-        chess.printBoard();
-
-        chess.move(6, 7, 5, 7);
-        chess.printBoard();
-
-        chess.move(0, 3, 2, 3);
-        chess.printBoard();
-
-        /* block castling 
-         * chess.move(6, 6, 5, 6);
+        /*
+         * castling (king side) (failed because king has moved)
+         * chess.move(0, 6, 2, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 5, 4, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(1, 6, 2, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(7, 4, 6, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 5, 1, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 5, 5, 4);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 4, 0, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(5, 4, 4, 3);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 5, 0, 4);
+         * chess.printBoard();
+         * 
+         * chess.move(4, 3, 3, 2);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 4, 0, 6);
          * chess.printBoard();
          */
-        /**/chess.move(4, 6, 3, 6);
-        /**/chess.printBoard();
 
-        chess.move(0, 4, 0, 2);
-        chess.printBoard();
+        /* castling (king side) (failed because rook has moved)
+         * chess.move(0, 6, 2, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 5, 4, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(1, 6, 2, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(7, 4, 6, 5);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 5, 1, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 5, 5, 4);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 7, 0, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(5, 4, 4, 3);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 6, 0, 7);
+         * chess.printBoard();
+         * 
+         * chess.move(4, 3, 3, 2);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 4, 0, 6);
+         * chess.printBoard();
+         */
 
+        /*
+         * castling (queen side)
+         * chess.move(0, 1, 2, 2);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 4, 4, 4);
+         * chess.printBoard();
+         * 
+         * chess.move(1, 3, 3, 3);
+         * chess.printBoard();
+         * 
+         * chess.move(7, 3, 4, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(1, 1, 3, 1);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 0, 5, 0);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 2, 2, 0);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 7, 5, 7);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 3, 2, 3);
+         * chess.printBoard();
+         * 
+         * chess.move(4, 6, 3, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 4, 0, 2);
+         * chess.printBoard();
+         */
+
+        /*
+         * castling (queen side) (blocked by opponent)
+         * chess.move(0, 1, 2, 2);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 4, 4, 4);
+         * chess.printBoard();
+         * 
+         * chess.move(1, 3, 3, 3);
+         * chess.printBoard();
+         * 
+         * chess.move(7, 3, 4, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(1, 1, 3, 1);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 0, 5, 0);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 2, 2, 0);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 7, 5, 7);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 3, 2, 3);
+         * chess.printBoard();
+         * 
+         * chess.move(6, 6, 5, 6);
+         * chess.printBoard();
+         * 
+         * chess.move(0, 4, 0, 2);
+         * chess.printBoard();
+         */
     }
 }
