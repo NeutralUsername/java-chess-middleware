86b372d94e5de8bd5a098de189675354ab812110
NeutralUsername <stuck.fabian@gmail.com>
2023-09-08 14:25:02 +0200

    more elaborate castling test case

--- Main.java
+++ Main.java
@@ -3,11 +3,64 @@ import chess.Chess;
 public class Main {
     public static void main(String[] args) {
 
-        Server server = new Server(4711);
-        if (server.getServerSocket() == null) {
-            return;
-        }
-        server.listenForConnections();
+        /*
+         * Server server = new Server(4711);
+         * if (server.getServerSocket() == null) {
+         * return;
+         * }
+         * server.listenForConnections();
+         */
+
+        Chess chess = new Chess();
+        chess.printBoard();
+
+        chess.move(1, 4, 2, 4);
+        chess.printBoard();
+
+        chess.move(6, 5, 5, 5);
+        chess.printBoard();
+
+        chess.move(1, 2, 2, 2);
+        chess.printBoard();
+
+        chess.move(6, 3, 5, 3);
+        chess.printBoard();
+
+        chess.move(0, 1, 2, 0);
+        chess.printBoard();
+
+        chess.move(7, 2, 3, 6);
+        chess.printBoard();
+
+        chess.move(0, 3, 2, 1);
+        chess.printBoard();
+
+        chess.move(6, 4, 4, 4);
+        chess.printBoard();
+
+        chess.move(1, 3, 2, 3);
+        chess.printBoard();
+
+        chess.move(6, 7, 5, 7);
+        chess.printBoard();
+
+        chess.move(0, 2, 1, 3);
+        chess.printBoard();
+
+        chess.move(7, 1, 5, 2);
+        chess.printBoard();
+
+        chess.move(0, 4, 0, 2); //fails because bishop is threatening square in between
+        chess.printBoard();
+
+        chess.move(0, 5, 1, 4); //success
+        chess.printBoard();
+
+        chess.move(5, 2, 3, 1);
+        chess.printBoard();
+
+        chess.move(0, 4, 0, 2);  //success because the bishop is no longer threatening the square in between
+        chess.printBoard();
 
     }
 }
@@ -187,8 +240,48 @@ public class Main {
  * chess.printBoard();
  */
 
+/* castling (queen side)
+ * chess.move(1, 4, 2, 4);
+ * chess.printBoard();
+ * 
+ * chess.move(6, 5, 5, 5);
+ * chess.printBoard();
+ * 
+ * chess.move(1, 2, 2, 2);
+ * chess.printBoard();
+ * 
+ * chess.move(6, 3, 5, 3);
+ * chess.printBoard();
+ * 
+ * chess.move(0, 1, 2, 0);
+ * chess.printBoard();
+ * 
+ * chess.move(7, 2, 3, 6);
+ * chess.printBoard();
+ * 
+ * chess.move(0, 3, 2, 1);
+ * chess.printBoard();
+ * 
+ * chess.move(6, 4, 4, 4);
+ * chess.printBoard();
+ * 
+ * chess.move(1, 3, 2, 3);
+ * chess.printBoard();
+ * 
+ * chess.move(3, 6, 4, 5);
+ * chess.printBoard();
+ * 
+ * chess.move(0, 2, 1, 3);
+ * chess.printBoard();
+ * 
+ * chess.move(7, 1, 5, 2);
+ * chess.printBoard();
+ * 
+ * chess.move(0, 4, 0, 2);
+ * chess.printBoard();
+ */
 /*
- * castling (queen side) (blocked by opponent)
+ * castling (queen side) (king in check after castling)
  * chess.move(0, 1, 2, 2);
  * chess.printBoard();
  * 
